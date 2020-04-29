a6ce015b6392f3343320f922cadd9d2b9f6d1661
Liu-xc <1392166232@qq.com>
2020-04-29 23:57:07 +0800

    good night

--- vue-music/src/components/basic/BottomPlayer.vue
+++ vue-music/src/components/basic/BottomPlayer.vue
@@ -1,12 +1,42 @@
 <template>
-  <div>BottomPlayer</div>
+  <div class="btm-player">
+    <div class="album-img">
+      <img :src="defaultImg" />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'BottomPlayer'
+  name: 'BottomPlayer',
+  data () {
+    return {
+      defaultImg: 'static/img/logo.png'
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="stylus">
+@import '~styles/var.styl'
+
+.btm-player
+  position fixed
+  bottom 0
+  width 100%
+  padding 0.35rem 0.2rem
+  background $bg-green
+  display flex
+  justify-content space-between
+  align-items center
+
+  .album-img
+    width 1rem
+    border-radius 50%
+    display flex
+    justify-content center
+    align-items center
+
+    img
+      width 100%
 </style>
